ea5c38af7f723c196a27fe1d5132f1e0ca87307c
Choi_Sungsik <sungsik97@naver.com>
2024-08-13 10:26:14 +0900

    2. 클래스 바인딩

---
+++ src/1. 라이프사이클 테스트.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>{{ count }}
+  </div>
+  <h1>Vue.js 라이프사이클 테스트</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count:0,
+    }
+  },
+  beforeCreate(){
+    console.log('LifeCycle is beforeCreate', this.count);
+  },
+  created(){
+    console.log('LifeCycle is created', this.count);
+    this.test()
+  },
+  // beforeMount() {
+  //   console.log('LifeCycle is beforeMount', document.querySelector('h1'));
+  // },
+  // mounted(){
+  //   console.log('LifeCycle is mounted', document.querySelector('h1'));
+  // },
+  methods:{
+    test() {
+      console.log('함수 호출');
+    },
+  },
+}
+
+// 뷰 기초문법 4
+// 1. 선언적 렌더링
+// 2. 클래스, 스타일바인딩
+// 3. 조건부 렌더링
+// 4. 리스트 렌더링
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ src/2. 클래스 바인딩.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>{{ rawHtml }}</div>   
+    <div>{{ rawHtml2 }}</div>
+    <h1 v-html="rawHtml2"></h1>
+
+    <h2 v-bind:class="{ active:isActive }">클래스 바인딩 테스트</h2>
+    <h2 :class="{ active:isActive}">클래스 바인딩 테스트</h2>
+    <button @click="change">버튼</button>
+
+    <h3 style="color: green; font-size: 20px">스타일 바인딩 테스트</h3>
+    <h3 :style="{color:fontColor, fontSize: fontSize + 'px'}">스타일 바인딩 테스트</h3>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            rawHtml: '텍스트 출력',
+            rawHtml2: '<span style="color:red"">텍스트 출력2</span>',
+            isActive: false,
+            fontColor: 'yellow',
+            fontSize: 20,
+        }
+    },
+    methods: {
+        change() {
+            this.isActive = !this.isActive
+        },
+    },
+}
+</script>
+<style scoped>
+h2.active {
+    color : blue;
+}
+</style>
