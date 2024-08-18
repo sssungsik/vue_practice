6b09f251f9ac7a5e5fa5ce15971c3282daaccedc
Choi_Sungsik <sungsik97@naver.com>
2024-08-19 00:11:53 +0900

    5-2 computed 까지

---
+++ src/5-1. 이벤트 핸들러.vue
@@ -0,0 +1,37 @@
+
+ <template>
+    <div id="event1">
+       <button @click="count++">인라인 핸들러</button>
+        <span> {{ count }}</span>
+        <br><br>
+        <button @click="isChange">메서드 핸들러</button>
+        <span> {{ name }}</span>
+    </div>
+
+
+    
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+         count:0,
+         name:'Vue.js',
+        }
+    },
+    methods: {
+        isChange() {
+            this.name = '변경'
+        }
+
+    }
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+ 
---
+++ src/5-2. Computed.vue
@@ -0,0 +1,48 @@
+
+<template>
+    <div id="event1">
+      <h1>{{text}}</h1>
+      <h1>함수 호출 값 : {{ isChange() }}</h1>
+      <h1>함수 호출 값 : {{ isChange() }}</h1>
+      <h1>함수 호출 값 : {{ isChange() }}</h1>
+      
+      <h2> {{ isComputed }}</h2>
+      <h2> {{ isComputed }}</h2>
+      <h2> {{ isComputed }}</h2>
+    </div>
+
+
+    
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        text:'Computed 테스트 데이터.',
+        }
+    },
+    computed: {
+        isComputed() {
+            console.log('computed 기능 생성');
+            
+            return this.text.split("").reverse().join('')
+        }
+    },
+    methods: {
+        isChange() {
+            console.log('함수 호출')
+            console.log(this.text)
+            return this.text.split("").reverse().join('')
+            
+            
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+ 
