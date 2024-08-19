839a17685edca319f36cd571507287efbe63ca37
Choi_Sungsik <sungsik97@naver.com>
2024-08-19 13:45:10 +0900

    5-3 watch

---
+++ src/5-3. Watch.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <button @click="isChange">{{ message }}</button>
+    <label>{{ watchMessage }}</label>
+
+
+    
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            message:"Watch 기능 테스트 오리지널.",
+            watchMessage:'',
+        }
+    },
+    watch: {
+        message() {
+            window.alert('message 변수 데이터 변경됨');
+            this.watchMessage = 'Watch 동작';
+        }
+    },
+    methods: {
+        isChange(){
+            console.log('함수 호출');
+            this.message = '변경된 message 데이터'
+            
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+ 
